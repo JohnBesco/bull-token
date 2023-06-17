8e8ece1248312c512ac3f6b0c54ae2a5219f8fdf
SirTokenSol <135555566+SirTokenSol@users.noreply.github.com>
2023-06-17 16:57:26 +0200

    Add color themes to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,17 @@ module.exports = {
       xl: '1440px'
     },
     extend: {
+      backgroundImage: {
+        'linear-green-one': 'linear-gradient(359.69deg, #064C35 0.26%, #064C35 19.11%, #0B5D39 70.5%, #14793F 105.62%)',
+      },
       colors: {
         darkGreen: '#02271B',
         lightGreen: '#278537',
         paleGreen: '#81CC40',
         brightGreen: '#19FB9B',
-        linearGreen1: 'linear-gradient(359.69deg, #064C35 0.26%, #064C35 19.11%, #0B5D39 70.5%, #14793F 105.62%)',
         linearGreen2: 'linear-gradient(90deg, #009245 0%, #19FB9B 33.33%, #00A8C5 66.67%, #73D83F 100%)',
         neonGreen: 'linear-gradient(180deg, #25AAE1 0%, rgba(64, 228, 149, 0.15) 33.33%, rgba(48, 221, 138, 0.1) 66.67%, rgba(43, 182, 115, 0) 100%)'
-      }
+      }, 
     },
   },
   plugins: [],
