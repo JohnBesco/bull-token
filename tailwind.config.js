8ec1c3594076b7213387188103fc854baad081e7
SirTokenSol <135555566+SirTokenSol@users.noreply.github.com>
2023-06-25 00:37:27 +0100

    Add hero section content and add text font to .json

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,11 +17,13 @@ module.exports = {
         lightGreen: '#278537',
         paleGreen: '#81CC40',
         brightGreen: '#19FB9B',
+        offWhite: '#FFFFFE',
         linearGreen2: 'linear-gradient(90deg, #009245 0%, #19FB9B 33.33%, #00A8C5 66.67%, #73D83F 100%)',
         neonGreen: 'linear-gradient(180deg, #25AAE1 0%, rgba(64, 228, 149, 0.15) 33.33%, rgba(48, 221, 138, 0.1) 66.67%, rgba(43, 182, 115, 0) 100%)'
       },
       fontFamily: {
-        mainFont: ['Alfa Slab One']
+        mainFont: ['Alfa Slab One'],
+        textFont: ['Gentium Book Plus']
       } 
     },
   },
