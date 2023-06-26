2cd70c1c082e2dda0a852f2d913ca415fe783ad4
SirTokenSol <135555566+SirTokenSol@users.noreply.github.com>
2023-06-26 23:36:08 +0100

    Add banner and contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,6 @@ module.exports = {
         paleGreen: '#81CC40',
         brightGreen: '#19FB9B',
         offWhite: '#FFFFFE',
-        linearGreen2: 'linear-gradient(90deg, #009245 0%, #19FB9B 33.33%, #00A8C5 66.67%, #73D83F 100%)',
         neonGreen: 'linear-gradient(180deg, #25AAE1 0%, rgba(64, 228, 149, 0.15) 33.33%, rgba(48, 221, 138, 0.1) 66.67%, rgba(43, 182, 115, 0) 100%)'
       },
       fontFamily: {
