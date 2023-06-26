7c9e24eb94aa566aa15b5d0c3c9625f9b18e63c6
SirTokenSol <135555566+SirTokenSol@users.noreply.github.com>
2023-06-27 00:49:04 +0100

    Add infinite scroll effect to banner

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ for (let i = 0; i < buttons.length; i++) {
     wrapper.classList.toggle("hidden");
   });
 }
+
+// Clone the banner items and append them to create the infinite scroll effect
+const bannerContainer = document.getElementById('banner-container');
+const bannerItems = bannerContainer.innerHTML;
+
+bannerContainer.innerHTML += bannerItems;
+
+// Scroll the banner container to the right continuously
+const scrollSpeed = 1; // Adjust this value to control the scroll speed
+let scrollAmount = 0;
+
+function scrollBanner() {
+  scrollAmount += scrollSpeed;
+
+  bannerContainer.scrollLeft = scrollAmount;
+
+  if (scrollAmount >= bannerContainer.scrollWidth / 2) {
+    scrollAmount = 0;
+  }
+}
+
+setInterval(scrollBanner, 20); // Adjust the interval time as per your preference
