7a731c40d8f61abe135de9e24d8f0e4a23ad42e5
SirTokenSol <johnbesco.dev@gmail.com>
2023-07-01 23:01:57 +0100

    Add javascript to create dropdown function for button

--- about.js
+++ about.js
@@ -7,6 +7,35 @@ for (let i = 0; i < buttons.length; i++) {
   });
 }
 
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected')
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+        });
+    });
+});
+
 gsap.from('.bounce', { duration: 1, y: '-200px', ease: 'bounce'});
 
 gsap.from('.header-item', { duration: 1, opacity: 0, stagger: 0.5 });
