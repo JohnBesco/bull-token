09a3bcf2b8b1af2e7a5cd5b78ff87d679699ce76
SirTokenSol <johnbesco.dev@gmail.com>
2023-07-02 19:29:45 +0100

    Add random float animation to about us img

--- about.js
+++ about.js
@@ -58,3 +58,59 @@ gsap.from('.enter-in-right', { duration: 2, x: '100vw', delay: 1.5, ease: "expo.
 
 gsap.from('.enter-in-left', { duration: 2, x: '-100vw', delay: 1.5, ease: "expo.out"});
 
+console.clear();
+
+const mainImg = document.querySelector(".main-img-wrapper > img");
+
+const randomX = random(10, 20);
+const randomY = random(20, 30);
+const randomDelay = random(0, 1);
+const randomTime = random(3, 5);
+const randomTime2 = random(5, 10);
+const randomAngle = random(8, 12);
+
+TweenLite.set(mainImg, {
+  x: randomX(-1),
+  y: randomX(1),
+  rotation: randomAngle(-1)
+});
+
+moveX(mainImg, 1);
+moveY(mainImg, -1);
+rotate(mainImg, 1);
+
+function rotate(target, direction) {
+  
+  TweenLite.to(target, randomTime2(), {
+    rotation: randomAngle(direction),
+    // delay: randomDelay(),
+    ease: Sine.easeInOut,
+    onComplete: rotate,
+    onCompleteParams: [target, direction * -1]
+  });
+}
+
+function moveX(target, direction) {
+  
+  TweenLite.to(target, randomTime(), {
+    x: randomX(direction),
+    ease: Sine.easeInOut,
+    onComplete: moveX,
+    onCompleteParams: [target, direction * -1]
+  });
+}
+
+function moveY(target, direction) {
+  
+  TweenLite.to(target, randomTime(), {
+    y: randomY(direction),
+    ease: Sine.easeInOut,
+    onComplete: moveY,
+    onCompleteParams: [target, direction * -1]
+  });
+}
+
+function random(min, max) {
+  const delta = max - min;
+  return (direction = 1) => (min + delta * Math.random()) * direction;
+}
